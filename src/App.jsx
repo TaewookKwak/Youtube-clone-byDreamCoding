ba764d38e7fa0fb1b1185e94c2159c59f71c6e21
Taewook <rhkrxodnr@gmail.com>
2022-01-04 11:48:50 +0900

    add youtube service

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,13 @@ import Search_header from './components/search_header/search_header'
 import VideoList from './components/video_list/video_list'
 import styles from './App.module.css'
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([])
   const search = (query) => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    }
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyATnJJe08YFVjwP6spvks6-sPL4xpTCuVg`,
-      requestOptions,
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId })),
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log('error', error))
+    youtube.search(query).then((videos) => setVideos(videos))
   }
   useEffect(() => {
-    const requestOptions = {
-      //이 부분은 fetch의 옵션을 전달해줌
-      method: 'GET',
-      redirect: 'follow',
-    }
-
-    fetch(
-      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyATnJJe08YFVjwP6spvks6-sPL4xpTCuVg',
-      requestOptions,
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error))
+    youtube.mostPopular().then((videos) => setVideos(videos))
   }, [])
   return (
     <div className={styles.app}>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import '@fortawesome/fontawesome-free/js/all.js'
-
+import Youtube from './service/youtube'
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
   document.getElementById('root'),
 )
