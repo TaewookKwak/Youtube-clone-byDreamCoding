861d66d226c5543af64a21a8bae6adcf0bfe8eb0
Taewook <rhkrxodnr@gmail.com>
2022-01-04 22:52:55 +0900

    fetch -> axios library

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,27 +1,45 @@
+import axios from 'axios'
+
 class Youtube {
   constructor(key) {
-    this.key = key
-    this.getRequestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    }
+    this.youtube = axios.create({
+      baseURL: 'https://youtube.googleapis.com/youtube/v3',
+      params: { key: key },
+    })
   }
 
   async mostPopular() {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-      this.getRequestOptions,
-    )
-    const result_1 = await response.json()
-    return result_1.items
+    const response = await this.youtube.get('videos', {
+      params: {
+        part: 'snippet',
+        chart: 'mostPopular',
+        maxResults: 25,
+      },
+    })
+    return response.data.items
+    // const response = await fetch(
+    //   `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+    //   this.getRequestOptions,
+    // )
+    // const result_1 = await response.json()
+    // return result_1.items
   }
   async search(query) {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
-      this.getRequestOptions,
-    )
-    const result_1 = await response.json()
-    return result_1.items.map((item) => ({ ...item, id: item.id.videoId }))
+    const response = await this.youtube.get('search', {
+      params: {
+        part: 'snippet',
+        chart: 'mostPopular',
+        maxResults: 25,
+        q: query,
+      },
+    })
+    return response.data.items.map((item) => ({ ...item, id: item.id.videoId }))
+    // const response = await fetch(
+    //   `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+    //   this.getRequestOptions,
+    // )
+    // const result_1 = await response.json()
+    // return result_1.items.map((item) => ({ ...item, id: item.id.videoId }))
   }
 }
 
